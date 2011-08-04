c643d79ff21dadb454152c7cda6dc01d24ac1db6
Action Ben <ben@actionhank.co.uk>
2011-08-04 22:41:21 +0100

    Add teaser page

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'maruku'
+
+configure :production do
+  before do
+    if request.host != 'www.wagsandwhiskers.co.uk'
+      redirect "http://www.wagsandwhiskers.co.uk"+request.fullpath, 301
+    end
+  end
+end
+
+get '/' do
+  haml :index
+end
+
+get '/stylesheets/front.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :'stylesheets/front'
+end
